e23934e8fcea9920c1b930d0c6a163b00b16ef40
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2019-02-08 09:46:44 +0100

    Transpile the code with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+const babel = require("rollup-plugin-babel");
+
 module.exports = {
   input: "src/index.js",
   externals: ["reacts"],
   output: {
     file: "dist/index.js",
     format: "cjs"
-  }
+  },
+  plugins: [babel()]
 };
